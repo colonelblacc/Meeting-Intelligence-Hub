2281900206a88849e53d5a860127442afc83b55a
AJITH <colonelblackshawn@gmail.com>
2026-04-08 00:08:26 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-cd backend
-call .venv\Scripts\python.exe -m pip install -r requirements.txt
-echo.
-echo Starting Meeting Intelligence Hub...
-echo You can access the application at http://localhost:8000
-call .venv\Scripts\uvicorn.exe main:app --host 0.0.0.0 --port 8000
-pause
